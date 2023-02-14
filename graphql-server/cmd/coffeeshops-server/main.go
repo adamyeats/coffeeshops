2f6902980428525d9e2289147fe3e7761c9d55c1
Adam Yeats <ay@xadamy.xyz>
2023-02-14 04:44:39 +0100

    Refactor main.go

--- graphql-server/cmd/coffeeshops-server/main.go
+++ graphql-server/cmd/coffeeshops-server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
@@ -15,39 +16,34 @@ import (
 // main is the entrypoint for the server
 // this is a bit of a mess right now
 func main() {
-	ctx := context.Background()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
 	srv, err := server.New(ctx, config.FromEnv())
-
 	if err != nil {
-		panic(err)
+		log.Fatal("Failed to create server:", err)
 	}
 
-	// LFG
-	if err := srv.Serve(); err != nil {
-		log.Fatal("There was an error whilst serving:", err)
-	}
+	go func() {
+		if err := srv.Serve(); err != nil && err != http.ErrServerClosed {
+			log.Fatal("Failed to serve:", err)
+		}
+	}()
 
-	// Wait for interrupt signal to gracefully shutdown the server with
-	// a timeout of 5 seconds.
+	// wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
 
 	log.Println("Server is shutting down...")
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel = context.WithTimeout(ctx, 5*time.Second)
 	defer cancel()
 
 	if err := srv.Stop(ctx); err != nil {
-		log.Fatal("There was an error whilst shutting down:", err)
+		log.Fatal("Failed to shutdown server:", err)
 	}
 
-	// catching ctx.Done() timeout of 5 seconds.
-	select {
-	case <-ctx.Done():
-		log.Println("timeout of 5 seconds.")
-	}
-
-	log.Println("Server exiting")
+	log.Println("Server exited gracefully")
 }
