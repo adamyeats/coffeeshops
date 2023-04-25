dd329e5eee93ede91d5a6ff3a616f1eec309a456
Adam Yeats <ay@xadamy.xyz>
2023-04-25 12:26:54 +0200

    Use REACT_APP_GRAPHQL_ENDPOINT in codegen config

--- app/codegen.ts
+++ app/codegen.ts
@@ -1,7 +1,7 @@
 import { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-  schema: 'http://localhost:8080/graphql',
+  schema: process.env.REACT_APP_GRAPHQL_ENDPOINT,
   documents: ['src/**/*.tsx'],
   ignoreNoDocuments: true, // for better experience with the watcher
   generates: {
